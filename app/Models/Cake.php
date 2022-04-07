b224056d0d187dda0ff988c582e117e254127c7a
muraokajade <kanemichi.jade@gmail.com>
2022-04-08 06:22:48 +0900

    first

---
+++ app/Models/Cake.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Ingredients;
+
+class Cake extends Model
+{
+    use HasFactory;
+
+    public function ingredients() {
+        return $this->hasMany(Indredients::class);
+    }
+}
